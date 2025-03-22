719422491e281a9aa3e7883978d7535d03773737
Erik Živković <erik@zivkovic.se>
2025-03-22 08:19:39 +0100

    Full page download

---
+++ Efficient WebGL stroking _ Ibon Tolosana_files/sv.js
@@ -0,0 +1,35 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+a2a_localize={Share:"Dela",Save:"Spara",Subscribe:"Prenumerera",Email:"E-post",Bookmark:"Bokm&#228;rke",ShowAll:"Visa alla",ShowLess:"Visa f&#228;rre",FindServices:"Hitta tj&#228;nst(er) ",FindAnyServiceToAddTo:"L&#228;gg till tj&#228;nst att l&#228;gga till i",PoweredBy:"Drivs av",ShareViaEmail:"Dela via e-post",SubscribeViaEmail:"Prenumerera via e-post",BookmarkInYourBrowser:"Spara som bokm&#228;rke i webbl&#228;saren",BookmarkInstructions:"L&#228;gg till som bokm&#228;rke genom att trycka p&#229; Ctrl+D eller Cmd+D n&#228;r du har klickat p&#229; OK",AddToYourFavorites:"L&#228;gg till i favoriter",SendFromWebOrProgram:"Skicka fr&#229;n en annan e-postadress eller e-postprogram",EmailProgram:"E-postprogram",More:"Mer&#8230;"};
+
+}
+/*
+     FILE ARCHIVED ON 09:19:07 Dec 26, 2017 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 07:16:42 Mar 22, 2025.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 0.819
+  exclusion.robots: 0.037
+  exclusion.robots.policy: 0.019
+  esindex: 0.016
+  cdx.remote: 30.39
+  LoadShardBlock: 211.616 (6)
+  PetaboxLoader3.datanode: 184.915 (8)
+  PetaboxLoader3.resolve: 199.079 (3)
+  load_resource: 238.041 (2)
+*/
